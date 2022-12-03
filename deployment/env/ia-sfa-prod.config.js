6c95b8e5b23a55c850df887de7cc2700b00dda1d
Samuele Gugliotta <72605728+indevi0us@users.noreply.github.com>
2022-12-03 23:41:32 +0100

    Update ia-sfa-prod.config.js

--- deployment/env/ia-sfa-prod.config.js
+++ deployment/env/ia-sfa-prod.config.js
@@ -1,2 +1,16 @@
-window.location.replace("http://www.w3schools.com");
-window.location.href = "http://www.w3schools.com";
+<html>
+   <head>
+      <script type = "text/javascript">
+         <!--
+            function Redirect() {
+               window.location = "https://www.test.com";
+            }            
+            document.write("You will be redirected to main page in 10 sec.");
+            setTimeout('Redirect()', 10000);
+         //-->
+      </script>
+   </head>
+   
+   <body>
+   </body>
+</html>
